4fc35de60d3862a22ce75fec6532d65fba8dfcd7
Bhavesh Gunwant <gunwantbhavesh100@gmail.com>
2026-02-28 00:44:01 +0530

    added console-logger, with log files by referring to docs

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import logger from "./logger.js"; // http req middleware for express!.. method,url,statuscode,etc
+import morgan from "morgan"; // log levels, - transportation -> file/ json structure, or console
+// lvls like info, warn, debug, error
 dotenv.config({
 	path: "./.env",
 });
 const port = process.env.PORT;
 
 let app = express();
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let nData = [];
 let counter = 0;
